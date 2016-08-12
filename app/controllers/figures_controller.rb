34a9948fac44e0731f99d3cc1f9f6cceb0d34a0d
Jesse Novotny <jessenovotny@gmail.com>
2016-08-12 13:40:51 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,14 +1,62 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do 
+    erb :'figures/index'
+  end
+
+
   get '/figures/new' do
-    binding.pry
     erb :'figures/new'
   end
 
-  post '/figures/new' do
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
 
-    #create a new figure, etc.
-    #go to the vigures view page
+  post '/figures/new' do
+    figure = Figure.create(name: params[:figure][:name])
+    ## Landmarks
+    if params[:figure].include?("landmark_ids")
+      params[:figure][:landmark_ids].each do |landmark_id|
+        figure.landmarks << Landmark.find(landmark_id) if !figure.landmarks.include?(Landmark.find(landmark_id))
+      end
+    end
+    figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name])if !params[:landmark][:name].empty? && !figure.landmarks.include?(Landmark.find_by(name: params[:landmark][:name]))
+    ## Titles
+    if params[:figure].include?("title_ids")
+      params[:figure][:title_ids].each do |title_id|
+        figure.titles << Title.find(title_id) if !figure.titles.include?(Title.find(title_id))
+      end
+    end
+    figure.titles << Title.find_or_create_by(name: params[:title][:name]) if !params[:title][:name].empty? && !figure.titles.include?(Title.find_by(name: params[:title][:name]))
+    figure.save
+    redirect "/figures/#{figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures/:id' do
+    figure = Figure.find(params[:id])
+    figure.name = params[:figure][:name] if !params[:figure][:name].empty?
+    if params[:figure].include?("landmark_ids")
+      params[:figure][:landmark_ids].each do |landmark_id|
+        figure.landmarks << Landmark.find(landmark_id) if !figure.landmarks.include?(Landmark.find(landmark_id))
+      end
+    end
+    figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name])if !params[:landmark][:name].empty? && !figure.landmarks.include?(Landmark.find_by(name: params[:landmark][:name]))
+    ## Titles
+    if params[:figure].include?("title_ids")
+      params[:figure][:title_ids].each do |title_id|
+        figure.titles << Title.find(title_id) if !figure.titles.include?(Title.find(title_id))
+      end
+    end
+    figure.titles << Title.find_or_create_by(name: params[:title][:name]) if !params[:title][:name].empty? && !figure.titles.include?(Title.find_by(name: params[:title][:name]))
+    figure.save
+    redirect "/figures/#{figure.id}"
   end
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,33 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks/new' do
+    landmark = Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    redirect "/landmarks/#{landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  post '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.name = params[:landmark][:name] if !params[:landmark][:name].empty?
+    landmark.year_completed = params[:landmark][:year_completed] if !params[:landmark][:year_completed].empty?
+    landmark.save
+    redirect "/landmarks/#{landmark.id}"
+  end
 end
